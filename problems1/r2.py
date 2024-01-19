15e2b7796414c4d24b0cb35a686214d10781281d
ragulprasanth1999 <rahulprasanth834@gmail.com>
2024-01-19 16:11:50 +0530

    ok

---
+++ problems1/r2.py
@@ -0,0 +1,5 @@
+i = int(input("Enter the input :"))
+if i % 2==0:
+    print(i, "is Even")
+else:
+    print(i,"is Odd" )
---
+++ problems1/r8.py
@@ -0,0 +1,6 @@
+# Remove nth occurance of the given word
+lis = ["geek","for","geek"]
+l1 = set(lis)
+list(l1)
+for i in l1:
+    print(i,lis.count(i))
